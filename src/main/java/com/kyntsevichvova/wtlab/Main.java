a7ffbcab8e2ef8dbe03c4e2a7d9b2df5fd0f9a34
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2019-12-15 15:19:01 +0300

    Init Lab2

--- src/main/java/com/kyntsevichvova/wtlab/Main.java
+++ src/main/java/com/kyntsevichvova/wtlab/Main.java
@@ -2,9 +2,12 @@ package com.kyntsevichvova.wtlab;
 
 import com.kyntsevichvova.wtlab.view.View;
 import com.kyntsevichvova.wtlab.view.factory.ViewFactory;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class Main {
     public static void main(String[] args) {
+        log.debug("Entered main");
         View view = ViewFactory.getInstance().getView();
         view.start();
     }
