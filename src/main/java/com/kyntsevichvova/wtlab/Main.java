c6b569d6c27f900156a3fc527c4d5ef36f755ae8
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2019-12-15 15:41:30 +0300

    Use lombok for beans

--- src/main/java/com/kyntsevichvova/wtlab/Main.java
+++ src/main/java/com/kyntsevichvova/wtlab/Main.java
@@ -8,6 +8,11 @@ import lombok.extern.log4j.Log4j2;
 public class Main {
     public static void main(String[] args) {
         log.debug("Entered main");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         View view = ViewFactory.getInstance().getView();
         view.start();
     }
--- src/main/java/com/kyntsevichvova/wtlab/bean/Horse.java
+++ src/main/java/com/kyntsevichvova/wtlab/bean/Horse.java
@@ -1,10 +1,16 @@
 package com.kyntsevichvova.wtlab.bean;
 
+import lombok.*;
+
 import java.io.Serializable;
 import java.util.Comparator;
 
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Horse extends BaseEntity implements Serializable, Comparable<Horse> {
-    private int id;
     private String name;
     private int wins;
 
@@ -12,30 +18,6 @@ public class Horse extends BaseEntity implements Serializable, Comparable<Horse>
             Comparator.comparing(Horse::getName).thenComparing(Horse::getWins);
     private final static Comparator<Horse> winsComparator =
             Comparator.comparing(Horse::getWins).thenComparing(Horse::getName);
-
-    public Horse() {
-    }
-
-    public Horse(String name, int wins) {
-        this.name = name;
-        this.wins = wins;
-    }
-
-    public int getWins() {
-        return wins;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setWins(int wins) {
-        this.wins = wins;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public int compareTo(Horse o) {
@@ -48,10 +30,5 @@ public class Horse extends BaseEntity implements Serializable, Comparable<Horse>
 
     public static Comparator<Horse> getWinsComparator() {
         return winsComparator;
-    }
-
-    @Override
-    public String toString() {
-        return "Horse: " + name + ", " + wins;
     }
 }
