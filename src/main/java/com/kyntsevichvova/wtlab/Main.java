6d52cbd77c3c5d7966162c08611ff70b810ba038
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2019-12-15 15:55:58 +0300

    Revert to Lab 1

--- src/main/java/com/kyntsevichvova/wtlab/Main.java
+++ src/main/java/com/kyntsevichvova/wtlab/Main.java
@@ -2,17 +2,9 @@ package com.kyntsevichvova.wtlab;
 
 import com.kyntsevichvova.wtlab.view.View;
 import com.kyntsevichvova.wtlab.view.factory.ViewFactory;
-import lombok.extern.log4j.Log4j2;
 
-@Log4j2
 public class Main {
     public static void main(String[] args) {
-        log.debug("Entered main");
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
         View view = ViewFactory.getInstance().getView();
         view.start();
     }
