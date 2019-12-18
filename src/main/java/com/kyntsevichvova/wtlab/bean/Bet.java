0062fa97a6dbd5445b1f1f649a67b6de07ead3e5
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2019-12-18 16:06:15 +0300

    Fix beans

--- src/main/java/com/kyntsevichvova/wtlab/bean/Bet.java
+++ src/main/java/com/kyntsevichvova/wtlab/bean/Bet.java
@@ -1,4 +1,24 @@
 package com.kyntsevichvova.wtlab.bean;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@NoArgsConstructor
+@JacksonXmlRootElement(localName = "bet")
 public class Bet extends BaseEntity {
+
+    private String user;
+
+    private Long raceId;
+
+    private Long horseId;
+
+    private Integer sum;
+
 }
--- src/main/java/com/kyntsevichvova/wtlab/bean/Race.java
+++ src/main/java/com/kyntsevichvova/wtlab/bean/Race.java
@@ -1,4 +1,22 @@
 package com.kyntsevichvova.wtlab.bean;
 
-public class Race extends BaseEntity  {
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@NoArgsConstructor
+@JacksonXmlRootElement(localName = "race")
+public class Race extends BaseEntity {
+
+    private Date date;
+
+    private Long winnerId;
+
 }
